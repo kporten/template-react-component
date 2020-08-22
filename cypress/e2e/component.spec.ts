f930293f6f176434dced9b6f735ca79bb98b1836
Kevin Porten <hello@kevinporten.dev>
2020-08-22 14:40:11 +0200

    test(cypress): improve example code

--- cypress/e2e/component.spec.ts
+++ cypress/e2e/component.spec.ts
@@ -1,9 +1,10 @@
+// https://storybook.js.org/docs/react/workflows/interaction-testing
 describe('Component', () => {
   beforeEach(() => {
-    cy.visit('iframe.html?id=component--basic&');
+    cy.visit('iframe.html?id=component--basic');
   });
 
   it('displays the correct headline', () => {
-    cy.findByText(/test/).should('be.visible');
+    cy.findByText('test').should('be.visible');
   });
 });
