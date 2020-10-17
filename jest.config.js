e3a6f6e5716dd2e9b5cef0b434d06db4fb0152cc
Kevin Porten <hello@kevinporten.dev>
2020-10-17 15:10:29 +0200

    chore: improve jest config and mocks

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 // https://jestjs.io/docs/en/configuration
 module.exports = {
-  collectCoverageFrom: ['src/lib/**/*.tsx', '!src/stories/**/*'],
+  collectCoverageFrom: ['src/lib/**/*.ts?(x)', '!src/stories/**/*'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+      '<rootDir>/__mocks__/file.mock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svg.mock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['./jest.setup.js', './__mocks__/matchMedia.mock.js'],
   testMatch: ['**/*.test.ts?(x)'],
 };
