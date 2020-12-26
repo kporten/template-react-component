5777507fbb40a7f1b40c5c5c9a1662d8d90a9c4a
Kevin Porten <hello@kevinporten.dev>
2020-12-26 09:22:53 +0100

    chore(package): update dependencies

--- src/lib/Component.tsx
+++ src/lib/Component.tsx
@@ -2,8 +2,8 @@ type ComponentProps = {
   headline: string;
 };
 
-const Component: React.FC<ComponentProps> = ({ headline }) => {
-  return <h1>{headline}</h1>;
-};
+const Component: React.FC<ComponentProps> = ({ headline }) => (
+  <h1>{headline}</h1>
+);
 
 export default Component;
