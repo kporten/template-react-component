1582c347ef25cf4c1b17822cb7d2c9cfc09cad13
Kevin Porten <hello@kevinporten.dev>
2021-01-10 10:52:58 +0100

    feat: enable StrictMode in example component

--- src/lib/Component.tsx
+++ src/lib/Component.tsx
@@ -1,3 +1,5 @@
+import { StrictMode } from 'react';
+
 import { ReactComponent as Heart } from './assets/Heart.svg';
 import { version } from '../../package.json';
 
@@ -6,7 +8,7 @@ type ComponentProps = {
 };
 
 const Component: React.FC<ComponentProps> = ({ headline }) => (
-  <>
+  <StrictMode>
     <link
       rel="stylesheet"
       href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
@@ -18,7 +20,7 @@ const Component: React.FC<ComponentProps> = ({ headline }) => (
       <Heart className="w-8 h-8 text-red-600 animate-pulse" />
       <div className="mt-4 text-gray-400">v{version}</div>
     </div>
-  </>
+  </StrictMode>
 );
 
 export default Component;
