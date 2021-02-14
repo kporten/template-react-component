8405e957e008f82469bcc62e57c84008d2525407
Kevin Porten <hello@kevinporten.dev>
2021-02-14 17:14:43 +0100

    refactor(lib): update example component styles

--- src/lib/Component.tsx
+++ src/lib/Component.tsx
@@ -13,8 +13,8 @@ const Component: React.FC<ComponentProps> = ({ headline }) => (
       rel="stylesheet"
       href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
     />
-    <div className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-red-600">
+    <div className="flex flex-col items-center p-8">
+      <h1 className="mb-2 font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-red-600">
         {headline}
       </h1>
       <Heart className="w-8 h-8 text-red-600 animate-pulse" />
